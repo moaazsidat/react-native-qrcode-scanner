a96da065b48714b378bdd19d7f26a7aeb74e81c4
James Nolan <j.nolan@otherwise.ch>
2017-05-20 21:52:19 +0200

    Cleanup: removed unused imports, unused styles and noops

--- index.js
+++ index.js
@@ -2,13 +2,9 @@
 
 import React, { Component, PropTypes } from 'react';
 import {
-  AppRegistry,
   StyleSheet,
   Dimensions,
-  Text,
-  Navigator,
   Vibration,
-  Linking,
   Animated,
   Easing,
   View
@@ -83,7 +79,6 @@ export default class QRCodeScanner extends Component {
         setTimeout(() => (this._setScanning(false)), this.props.reactivateTimeout);
       }
     }
-    return;
   }
 
   _renderTopContent() {
@@ -155,28 +150,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     width: Dimensions.get('window').width,
-  },
-
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777',
-  },
-
-  textBold: {
-    fontWeight: '500',
-    color: '#000',
-  },
-
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)',
-  },
-
-  buttonTouchable: {
-    // backgroundColor: 'pink',
-    padding: 16,
   },
 
   camera: {
