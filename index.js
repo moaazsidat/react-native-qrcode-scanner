3479bad62cc5e0ab3eb0ee1573152351dc22c864
rekha <rekha110254@gmail.com>
2017-11-25 12:25:14 +0530

    commented duplicate props

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ export default class QRCodeScanner extends Component {
     reactivateTimeout: PropTypes.number,
     fadeIn: PropTypes.bool,
     showMarker: PropTypes.bool,
-    cameraType: PropTypes.oneOf(['front','back']),
+    // cameraType: PropTypes.oneOf(['front','back']),
     customMarker: PropTypes.element,
     containerStyle: PropTypes.any,
     cameraStyle: PropTypes.any,
