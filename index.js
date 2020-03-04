86cc7ca74649717bec05bd541f36a63e07a332d4
Claudio Alessi <smoppy@gmail.com>
2020-03-04 18:23:40 +0100

    Fix regression with PR #222

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ export default class QRCodeScanner extends Component {
         style={[styles.camera, this.props.cameraStyle]}
         onBarCodeRead={this._handleBarCodeRead.bind(this)}
         type={this.props.cameraType}
-        flashMode={this.state.flashMode}
+        flashMode={this.props.flashMode}
         captureAudio={false}
         {...this.props.cameraProps}
       >
