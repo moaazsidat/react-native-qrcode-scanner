30cbb106109a9b7145e27aec5e1be4c3dd2530a2
Roger Cassares <46907805+roger10cassares@users.noreply.github.com>
2021-03-17 22:40:44 -0300

    Create FunctionalComponentExample.js
    Hi @moaazsidat ! 
    
    Thanks for these great package. Since I am developing a new project,  I have implemented a working Example with Functional Component based with Hooks!
    
    As you can see, this code reads any QRCode message with intervals between 3000ms and shows the response in BottomContent Props. Please note that i have commented the Linking.openURL lines just to read any message.
    
    As it implements Hooks, it may be easier and faster and accoreding to the new React directives. PLease test and let me see if it everything okay! 
    
    I guess everything is correct also with `use strict' tag. I hope you could add this to the examples and help many people! Thanks man! I really appreciate.
    
    
    Best Regards,
    Rogério Cassares Pires

---
+++ FunctionalComponentExample.js
@@ -0,0 +1,73 @@
+'use strict';
+
+import React, { useState, useEffect } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking,
+  View,
+} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+
+const ScanQRCodeScreen = () => {
+  const [qrcodeGetData, setQRCodeGetData] = useState(null);
+  
+  useEffect(() => {
+  },[qrcodeGetData]);
+  
+  onSuccess = (e) => {
+    // Linking.openURL(e.data).catch(err =>
+    //   console.error('An error occured', err)
+    // );  
+    setQRCodeGetData(e.data);
+  }
+
+  console.log(`AFTER setQRCodeGetData ${qrcodeGetData}`);
+
+  return (
+    <QRCodeScanner
+      onRead={onSuccess}   
+      reactivate={true}
+      reactivateTimeout={3000}     
+      topContent={
+        <Text style={styles.centerText}>
+          Go to{' '}
+          <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+          your computer and scan the QR code.
+        </Text>
+      }
+      bottomContent={
+        <TouchableOpacity style={styles.buttonTouchable}>
+          <Text style={styles.buttonText}>OK. Got it!</Text>
+          <Text style={styles.buttonText}>QRCode data: {qrcodeGetData}</Text>
+        </TouchableOpacity>
+      }
+    />
+  );
+}
+export default ScanQRCodeScreen;
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+    textAlign: 'center',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
